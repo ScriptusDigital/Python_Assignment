fa56e08178615e350cd35b0caa765e8a3e248da9
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-03-29 10:18:43 +0100

    chore: Template and doc setup

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, flash 
+import random
+
 app = Flask(__name__)
+
+
+
+
 @app.route('/')
 def home():
-    return 'Test Succesful'
+    return render_template('index.html', title="Home")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    debug_flag = str(os.environ.get("DEBUG", "False")).lower() in ("1", "true", "yes")
+    app.run(debug=debug_flag)
     
+
+
