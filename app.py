dfb20ab1e29d81905c9742f6418606fe7a57a156
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-03-29 19:08:42 +0100

    chore: connecting pages

--- app.py
+++ app.py
@@ -16,13 +16,21 @@ def home():
     return render_template('index.html', title="Home")
 
 
-@app.route('/')
+@app.route('/contact')
 def contact():
     return render_template('contact.html', title="Contact")
 
-@app.route('/')
+@app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
+@app.route('/random')
+def random():
+    return render_template('random.html', title="Random")
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html', title="Find a Film")
 
 if __name__ == "__main__":
     debug_flag = str(os.environ.get("DEBUG", "False")).lower() in ("1", "true", "yes")
